234fcf9ceb6ca210ef1b64a3005a1dec9ea765c5
Dave Carlson <davicar319@acm.org>
2015-10-31 06:50:40 -0500

    Added TodoList.  rq02.3 passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :todo_lists
+  resources :todo_lists
   resources :profiles
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
