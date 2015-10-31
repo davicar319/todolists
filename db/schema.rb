c686f99ae4d243ccaaf7299e056d8a37fc9bf41d
Dave Carlson <davicar319@acm.org>
2015-10-31 06:46:29 -0500

    Profile added rq02.2 passes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030121506) do
+ActiveRecord::Schema.define(version: 20151031114435) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "gender"
+    t.integer  "birth_year"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
