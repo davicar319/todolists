986566e7cc947df6f9d42db417f6e60dd24e0284
Dave Carlson <davicar319@acm.org>
2015-10-31 08:38:14 -0500

    Implemented delete_todolist method.  rq08.2 now passes.

--- assignment/assignment.rb
+++ assignment/assignment.rb
@@ -73,14 +73,10 @@ class Assignment
   #
   def delete_user(id)
     User.destroy(id)
-      # accept an id input parameter
-      # use the User Model class to remove the User associated with the `id` primary key from the database
-      # (no return is required)
   end 
 
   def delete_todolist(id)
-      # accept an id input parameter
-      # use the TodoList Model class to get the TodoList associated with the `id` primary key.
-      # (no return is required)
+    TodoList.destroy(id)
   end
+
 end
